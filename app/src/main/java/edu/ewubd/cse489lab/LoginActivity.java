4ba298b90e5df6ca1a19820f953086d37b243dc5
Likhon <likhon.929@gmail.com>
2024-11-05 17:02:57 +0600

    UI

--- app/src/main/java/edu/ewubd/cse489lab/LoginActivity.java
+++ app/src/main/java/edu/ewubd/cse489lab/LoginActivity.java
@@ -1,4 +1,4 @@
-package edu.ewubd.cse489lab;
+package edu.ewubd.cse489_2021_2_60_071;
 
 import android.os.Bundle;
 
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Assignment_Task2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_assignment_task2);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
