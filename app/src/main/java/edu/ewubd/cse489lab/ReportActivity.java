e58c4eb17d1e73eef879ed4951fd896920721ff8
Likhon <likhon.929@gmail.com>
2025-01-13 23:03:09 +0600

    v2.3 search error fixed

--- app/src/main/java/edu/ewubd/cse489lab/ReportActivity.java
+++ app/src/main/java/edu/ewubd/cse489lab/ReportActivity.java
@@ -86,42 +86,30 @@ public class ReportActivity extends AppCompatActivity {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(ReportActivity.this, ReportActivity.class);
-                i.putExtra("SEARCH", etSearch.getText().toString().trim());
-                startActivity(i);
+                String searchBy = etSearch.getText().toString().trim();
+                loadLocalData(searchBy);
             }
         });
 
     }
 
-    public void onStart(){
+    public void onStart() {
         super.onStart();
+        loadLocalData("");
         loadRemoteData();
-        Intent i = getIntent();
-        if (i.hasExtra("SEARCH")){
-            String searchBy = getIntent().getStringExtra("SEARCH");
-            etSearch.setText(searchBy);
-            loadLocalData(searchBy);
-            System.out.println("if");
-        } else {
-            System.out.println("else");
-            loadLocalData("");
-        }
     }
-    private void loadLocalData(String searchBy){
+
+    private void loadLocalData(String searchBy) {
         items.clear();
         double totalCost = 0;
         ItemDB db = new ItemDB(this);
         String q = "SELECT * FROM items";
-        if(!searchBy.isEmpty()){
-            if (searchBy.matches("\\d{2}-\\d{2}-\\d{4}")) {
-                q += " WHERE date="+getDateInMilliSecond(searchBy);
-            } else {
-                q += " WHERE itemName LIKE '%"+searchBy+"%'";
-            }
+        if (!searchBy.isEmpty()) {
+            long date = getDateInMilliSecond(searchBy);
+            q += " WHERE itemName LIKE '%" + searchBy + "%' OR date LIKE '%" + date + "%'";
         }
         Cursor c = db.selectItems(q);
-        while (c.moveToNext()){
+        while (c.moveToNext()) {
             String id = c.getString(0);
             String itemName = c.getString(1);
             double cost = c.getDouble(2);
@@ -139,47 +127,48 @@ public class ReportActivity extends AppCompatActivity {
         tvTotalCost.setText(String.valueOf(totalCost));
     }
 
-    private void loadRemoteData(){
-        String keys[] = {"action","sid","semester"};
-        String values[] ={"restore", "2021-2-60-071","2024-3"};
+    private void loadRemoteData() {
+        String keys[] = {"action", "sid", "semester"};
+        String values[] = {"restore", "2021-2-60-071", "2024-3"};
         httpRequest(keys, values);
     }
 
-    private void httpRequest(final String keys[],final String values[]){
-        new AsyncTask<Void,Void,String>(){
+    private void httpRequest(final String keys[], final String values[]) {
+        new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... voids) {
-                List<NameValuePair> params=new ArrayList<NameValuePair >();
-                for (int i=0; i<keys.length; i++){
-                    params.add(new BasicNameValuePair(keys[i],values[i]));
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                for (int i = 0; i < keys.length; i++) {
+                    params.add(new BasicNameValuePair(keys[i], values[i]));
                 }
-                String url= "https://www.muthosoft.com/univ/cse489/index.php";
+                String url = "https://www.muthosoft.com/univ/cse489/index.php";
                 try {
-                    String data= RemoteAccess.getInstance().makeHttpRequest(url,"POST",params);
+                    String data = RemoteAccess.getInstance().makeHttpRequest(url, "POST", params);
                     return data;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 return null;
             }
-            protected void onPostExecute(String data){
-                if(data!=null){
+
+            protected void onPostExecute(String data) {
+                if (data != null) {
                     updateLocalDBByServerData(data);
                 }
             }
         }.execute();
     }
 
-    private void updateLocalDBByServerData (String data){
+    private void updateLocalDBByServerData(String data) {
         System.out.println("found");
-        try{
+        try {
             JSONObject jo = new JSONObject(data);
-            if(jo.has("classes")){
+            if (jo.has("classes")) {
                 items.clear();
                 double totalCost = 0;
                 JSONArray ja = jo.getJSONArray("classes");
                 ItemDB db = new ItemDB(this);
-                for(int i=0; i<ja.length(); i++){
+                for (int i = 0; i < ja.length(); i++) {
                     JSONObject item = ja.getJSONObject(i);
                     String id = item.getString("id");
                     String itemName = item.getString("itemName");
@@ -192,22 +181,42 @@ public class ReportActivity extends AppCompatActivity {
                     // Write code here to insert lecture information in SQL Database
                     db.updateItem(id, itemName, cost, date);
                 }
-                //db.close();
+                db.close();
                 adapter.notifyDataSetChanged();
                 tvTotalCost.setText(String.valueOf(totalCost));
             }
-        }catch(Exception e){}
+        } catch (Exception e) {
+        }
     }
-    private long getDateInMilliSecond(String date){
-        String[] dateParts = date.split("-");
-        int year = Integer.parseInt(dateParts[2]);
-        int month = Integer.parseInt(dateParts[1]) - 1; // Calendar.MONTH is zero-based
-        int day = Integer.parseInt(dateParts[0]);
-        Calendar currentCal = Calendar.getInstance();
-        currentCal.setTimeInMillis(0);
-        currentCal.set(Calendar.YEAR, year);
-        currentCal.set(Calendar.MONTH, month);
-        currentCal.set(Calendar.DATE, day);
-        return currentCal.getTimeInMillis();
+
+    private boolean deleteRemoteData(String deleteId) {
+        String[] keys = {"action", "sid", "semester", "id"};
+        String[] values = {"remove", "2021-2-60-071", "2024-3", deleteId};
+        try {
+            ItemDB db = new ItemDB(ReportActivity.this);
+            db.deleteItem(deleteId);
+            db.close();
+            httpRequest(keys, values);
+            onStart();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private long getDateInMilliSecond(String date) {
+        try {
+            String[] dateParts = date.split("-");
+            Calendar currentCal = Calendar.getInstance();
+            currentCal.setTimeInMillis(0);
+            currentCal.set(Calendar.YEAR, Integer.parseInt(dateParts[2]));
+            currentCal.set(Calendar.MONTH, Integer.parseInt(dateParts[1]) - 1); // Calendar.MONTH is zero-based
+            currentCal.set(Calendar.DATE, Integer.parseInt(dateParts[0]));
+            return currentCal.getTimeInMillis();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1; // Return a default value or handle the error as needed
+        }
     }
 }
